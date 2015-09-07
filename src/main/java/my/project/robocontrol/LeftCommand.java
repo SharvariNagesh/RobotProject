e6e649bc0c555c9113075d4a4638fe19b7902fdd
SharvariNagesh <sharvari.nagesh@gmail.com>
2015-09-07 10:53:16 +1000

    Implementation of LeftCommand to call Robot's turnLeft method

--- src/main/java/my/project/robocontrol/LeftCommand.java
+++ src/main/java/my/project/robocontrol/LeftCommand.java
@@ -1,13 +1,23 @@
 package my.project.robocontrol;
 
+/**
+ * 
+ * @author Sharvari Nagesh
+ * The LeftCommand is an implementation of command "LEFT".
+ * It checks if the Robot is placed. If it is placed, then, it calls the Robot's function to turn it left.
+ *
+ */
 public class LeftCommand implements RoboCommands{
 private Robot robot;
-private String command;
+
     public LeftCommand(Robot robot, String placeCommand)
     {
+    	this.robot = robot;
     }
 	public void execute()
 	{
+		
+			robot.turnLeft();
 		
 	}
 }
