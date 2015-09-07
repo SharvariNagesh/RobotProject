3d28f442b80a4d119e53c4459235f6529b62b4a4
SharvariNagesh <sharvari.nagesh@gmail.com>
2015-09-07 11:28:07 +1000

    Added Unit Test cases for RightCommand Implementation

---
+++ src/main/java/my/project/robocontrol/RightCommand.java
@@ -0,0 +1,14 @@
+package my.project.robocontrol;
+
+public class RightCommand implements RoboCommands {
+private Robot robot;
+
+	public RightCommand(Robot robot, String command)
+	{
+	
+	}
+	public void execute()
+	{
+	
+	}
+}
---
+++ src/test/java/my/project/robocontrol/RightCommandTest.java
@@ -0,0 +1,32 @@
+package my.project.robocontrol;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RightCommandTest {
+	private Robot robot;
+	private RoboCommands placeCmd;
+	private RoboCommands rightCmd;
+	
+	@Before
+	public void setUp() throws Exception {
+		robot = new Robot();
+		placeCmd = new PlaceCommand(robot, "PLACE 5,5,NORTH");
+		rightCmd = new RightCommand(robot,"RIGHT");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testForRobotLeftTurnTest() {
+		placeCmd.execute();
+		rightCmd.execute();
+		assertEquals("5,5 EAST", robot.report());
+	}
+
+}
