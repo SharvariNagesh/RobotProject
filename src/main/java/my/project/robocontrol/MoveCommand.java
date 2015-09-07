3e68196a6a84f32d47b02f6596dfd162114df559
SharvariNagesh <sharvari.nagesh@gmail.com>
2015-09-07 18:50:03 +1000

    Added code to move robot in MoveCommand implementation

--- src/main/java/my/project/robocontrol/MoveCommand.java
+++ src/main/java/my/project/robocontrol/MoveCommand.java
@@ -1,13 +1,24 @@
 package my.project.robocontrol;
 
+/**
+ * 
+ * @author Sharvari Nagesh
+ * MoveCommand implements the RoboCommands interface. It takes care of calling the move function of Robot
+ * Before calling the Robot's move function, this class makes sure, the move is valid, within the table
+ * and all the players involved are properly set.
+ */
 public class MoveCommand implements RoboCommands {
+	private Robot robot;
+	private String command;
 	
   public MoveCommand(Robot robot, String command)
   {
-	
+	this.robot = robot;
+	this.command = command;
   }
   public String execute()
   {
+	 robot.move();
 	return null;
   }
 }
